9515c72ea533df650f0a3bdbfbdc093d52ced35d
Romain <romain.sanchez@libre-informatique.fr>
2017-06-22 16:10:17 +0200

    Fix modal management

--- js/modules/baseUi/baseUi.js
+++ js/modules/baseUi/baseUi.js
@@ -16,23 +16,42 @@ app.register({
             app.core.ui.addTemplate('content', 'settings', 'js/modules/baseUi/views/settings.html');
         },
 
-        openModal: function(templateName, data) {
-            app.core.ctrl.render(templateName, data, false).then(function() {
-                $('.modal')
-                    .modal({
-                        dismissible: true,
-                        opacity: .5,
-                        inDuration: 300,
-                        outDuration: 200,
-                        startingTop: '4%',
-                        endingTop: '10%',
-                    })
-                    .modal('open');
-            });
+        openModal: function(templateName, data, options) {
+            var defaults = {
+                dismissible: true,
+                opacity: .5,
+                inDuration: 300,
+                outDuration: 200,
+                startingTop: '4%',
+                endingTop: '10%',
+            };
+
+            $.extend({}, defaults, options);
+
+            var open = function() {
+              var modal = $('.modal');
+
+              modal.modal();
+
+              if(!modal.hasClass('open')) {
+                  modal.modal('open');
+              }
+            }
+
+            if($('.modal').length == 0) {
+                app.core.ctrl.render(templateName, data, false).then(open);
+            } else {
+                open();
+            }
         },
 
         closeModal: function() {
-            $('.modal').modal('close');
+            var modal = $('.modal');
+
+            if(modal.hasClass('open')) {
+                modal.modal('close');
+                $('.modal').remove();
+            }
         },
     }
 });
