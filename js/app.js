3413230d2afa20066cb73f98ac5081e46a971768
Romain Sanchez <romainsancho@gmail.com>
2020-06-08 16:29:39 +0200

    Refactoring

--- js/app.js
+++ js/app.js
@@ -4,94 +4,77 @@ let app = {
     total: 18,
     // Nombre de paires actuel
     score: 0,
+    // Jeu de cartes
+    set: [],
     // Cartes sélectionnées
     firstCard: undefined,
     secondCard: undefined,
-    // Jeu de cartes
-    cards: [
-        { name: 'apple',         image: 'apple.png' },
-        { name: 'apricot',       image: 'apricot.png' },
-        { name: 'banana',        image: 'banana.png' },
-        { name: 'cherry',        image: 'cherry.png' },
-        { name: 'grape',         image: 'grape.png' },
-        { name: 'green-apple',   image: 'green-apple.png' },
-        { name: 'lemon',         image: 'lemon.png' },
-        { name: 'lime',          image: 'lime.png' },
-        { name: 'mango',         image: 'mango.png' },
-        { name: 'peach',         image: 'peach.png' },
-        { name: 'pear',          image: 'pear.png' },
-        { name: 'plum',          image: 'plum.png' },
-        { name: 'pomegranate',   image: 'pomegranate.png' },
-        { name: 'raspberry',     image: 'raspberry.png' },
-        { name: 'strawberry',    image: 'strawberry.png' },
-        { name: 'tangerine',     image: 'tangerine.png' },
-        { name: 'watermelon',    image: 'watermelon.png' },
-        { name: 'yellow-cherry', image: 'yellow-cherry.png' },
-    ],
+    // Initialisation de l'application
+    init: () => {
+        app.ui.clickEvents();
+    },
 
     // Lancement d'une partie
     startGame: () => {
-        app.initBoard();  
+        // Récupération du jeu de cartes mélangé
+        app.set = app.cards.getShuffledSet();
+
+        // Initialisation du jeu de carte dans l'interface
+        app.ui.initBoard();  
       
         // Lancement du timer de la partie
         setTimeout(() => {
         //     endGame();
         }, 50000);
     }, 
-    // Initialisation du tableau de jeux
-    initBoard: () => {
-        // Réinitialisation si ce n'est pas la première partie
-        $('#board').empty();
-        
-        let pairs = app.utils.createPairs();
-        app.utils.shufflePairs(pairs);
-        
-        // Insertion des cartes HTML dans le DOM
-        $.each(pairs, app.ui.addCard);
-        
-    },
 
     // Gestion de la sélection d'une carte
     handleSelection: (event) => {
         // Récupération du nom de la carte cliquée
         let cardName = $(event.currentTarget).attr('id');
         // Recherche de la carte dans le jeu de carte
-        let card = app.cards.find((element) => {
+        let card = app.set.find((element) => {
             return element.name == cardName;
         });
 
         if(app.firstCard === undefined) {
             app.ui.flipCard(card);
             app.firstCard = card;
+
             return;
         }
 
         if(app.secondCard === undefined) {
             app.ui.flipCard(card);
             app.secondCard = card;
-            
+            // Validation de la paire sélectionnée         
             app.checkSelection();
+
         }
     },
-    // Valide la paire sélectionnée
+    // Compare les cartes sélectionnées
     checkSelection: () => {
-        if(app.firstCard.name === app.secondCard.name) {
+        // On compare les images des cartes car les noms sont toujours différents
+        if(app.firstCard.image === app.secondCard.image) {
             app.increaseScore();
-            app.ui.disableSelection();
+            //app.ui.disableSelection();
+            app.resetSelection(true);
 
             return;
         }
 
-        app.resetSelection();
+        app.resetSelection(false);
     },
 
     // Réinitialise la sélection
-    resetSelection: () => {
-        app.ui.flipCard(app.firstCard);
-        app.firstCard === undefined;
-
-        app.ui.flipCard(app.secondCard);
-        app.secondCard === undefined;
+    resetSelection: (valid) => {
+        if (!valid) {
+            app.ui.flipCard(app.firstCard);
+            app.ui.flipCard(app.secondCard);
+        }
+        
+        app.firstCard = undefined;
+        app.secondCard = undefined;
     },
 
     // Augmente le score quand une paire est validée
@@ -114,10 +97,5 @@ let app = {
     },
 } 
 
-// EVENEMENTS
-$(document)
-    // Click du bouton "Démarrer"
-    .on('click', '#start-button', app.startGame)
-    // Click sur une carte
-    .on('click', '.card', app.handleSelection) 
-;
+// Lancement de l'application
+$(document).ready(app.init);
---
+++ js/cards.js
@@ -0,0 +1,56 @@
+// Génération du jeu de cartes
+app.cards = {
+    // Liste des cartes existantes
+    initialSet: [
+        { name: 'apple',         image: 'apple.png' },
+        { name: 'apricot',       image: 'apricot.png' },
+        { name: 'banana',        image: 'banana.png' },
+        { name: 'cherry',        image: 'cherry.png' },
+        { name: 'grape',         image: 'grape.png' },
+        { name: 'green-apple',   image: 'green-apple.png' },
+        { name: 'lemon',         image: 'lemon.png' },
+        { name: 'lime',          image: 'lime.png' },
+        { name: 'mango',         image: 'mango.png' },
+        { name: 'peach',         image: 'peach.png' },
+        { name: 'pear',          image: 'pear.png' },
+        { name: 'plum',          image: 'plum.png' },
+        { name: 'pomegranate',   image: 'pomegranate.png' },
+        { name: 'raspberry',     image: 'raspberry.png' },
+        { name: 'strawberry',    image: 'strawberry.png' },
+        { name: 'tangerine',     image: 'tangerine.png' },
+        { name: 'watermelon',    image: 'watermelon.png' },
+        { name: 'yellow-cherry', image: 'yellow-cherry.png' },
+    ],
+
+    // Récupération du jeu de cartes complet mélangé aléatoirement
+    getShuffledSet: () => {
+        let pairs = app.cards.createPairs();
+
+        app.cards.shuffle(pairs);
+        
+        return pairs;
+    },
+    // Dédoublement des cartes pour pouvoir faire des paires
+    createPairs: () => {
+        let pairs = [];
+
+        $.each(app.cards.initialSet, (key, card) => {
+            let clone = {
+                name: `${card.name}2`,
+                image: card.image
+            };
+
+            pairs.push(card);
+            pairs.push(clone);
+        });
+
+        return pairs;
+    },
+
+    // Mélange aléatoire des cartes
+    shuffle: (pairs) => {
+        // pairs.sort(() => {
+        //     return 0.5 - Math.random() 
+        // });
+    }
+}
