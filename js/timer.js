4fca0c616f64bcd07a035421c8f1c4e134886358
Romain Sanchez <romainsancho@gmail.com>
2020-06-11 15:02:31 +0200

    Add game timer

---
+++ js/timer.js
@@ -0,0 +1,42 @@
+// Compteur de temps
+app.timer = {
+    interval: undefined,
+    countFrom: 30, // seconds
+    count: undefined,
+
+    //Lancement et redémarrage du timer
+    start: () => {
+      if (app.timer.interval) {
+        clearInterval(app.timer.interval);
+      }
+
+      app.timer.count = app.timer.countFrom;
+
+      app.timer.count = app.timer.countFrom;
+      app.timer.interval = setInterval((app.timer.tick).bind(app.timer), 1000);
+    },
+
+    // Arret du timer
+    stop: () => {        
+      clearInterval(app.timer.interval);       
+    },
+
+    // Temps restant
+    getTime: () => {
+        return app.timer.count;
+    },
+
+    // Décrémentation du compteur
+    tick: () => {
+      app.timer.count -= 1;
+
+      if (app.timer.count<= 0) {
+        app.timer.count = 0;
+
+        clearInterval(app.timer.interval);
+        $(document).trigger('timer:done');
+       }
+
+       $(document).trigger('timer:tick', app.timer.count);
+    }
+};
